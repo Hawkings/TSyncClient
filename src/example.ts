f523f3b3f3b341b0a2260b9fdc372486960df617
lilezek <devezek@gmail.com>
2017-02-13 13:32:31 +0100

    Better code.

--- src/example.ts
+++ src/example.ts
@@ -1,14 +1,13 @@
 import {Client} from './core/Client';
-import {Remote, Member, RemoteObject} from './sharedobject/Remote';
+import {Remote, Member, RemoteObject, RemoteUtils as rUtils} from './sharedobject/Remote';
 import {Validators as RV} from './sharedobject/Validators';
-import EventEmitter = require('wolfy87-eventemitter');
 
 var mySlide: HTMLInputElement = <HTMLInputElement>document.getElementById('client');
 var serverSlide: HTMLInputElement = <HTMLInputElement>document.getElementById('server');
 
 @Remote
 class ExampleSyncedRanger {
-  @Member(RV.isNumericString.isGreatOrEqual(0).isLessOrEqual(100))
+  @Member(RV.isNumericString.isGreaterOrEqual(0).isLessOrEqual(100))
   public value: string;
 }
 
@@ -20,13 +19,12 @@ var myRanger: ExampleSyncedRanger&RemoteObject;
 var serverRanger: ExampleSyncedRanger&RemoteObject;
 
 c.on('newObject', (object) => {
-  if (object.__remoteInstance.id === "clientRange") {
+  if (rUtils.GetId(object) === "clientRange") {
     myRanger = object;
-  } else if (object.__remoteInstance.id == "serverRange") {
+  } else if (rUtils.GetId(object) === "serverRange") {
     serverRanger = object;
   }
   if (myRanger && serverRanger) {
-    console.log(serverRanger);
     mySlide.onchange = function(v) {
       myRanger.value = mySlide.value;
     }
--- src/example2.ts
+++ src/example2.ts
@@ -1,13 +1,12 @@
 import {Client} from './core/Client';
-import {Remote, Member, RemoteObject} from './sharedobject/Remote'
+import {Remote, Member, RemoteObject, RemoteUtils as rUtils} from './sharedobject/Remote'
 import {Validators as RV} from './sharedobject/Validators'
-import EventEmitter = require("wolfy87-eventemitter");
 
 @Remote
 class ExampleMouse {
-  @Member(RV.isNumber.isGreatOrEqual(0).isInteger)
+  @Member(RV.isNumber.isGreaterOrEqual(0).isInteger)
   public x: number;
-  @Member(RV.isNumber.isGreatOrEqual(0).isInteger)
+  @Member(RV.isNumber.isGreaterOrEqual(0).isInteger)
   public y: number;
 }
 
@@ -16,11 +15,9 @@ var c = new Client({
 })
 
 var body = document.body;
-var images: { [id: string]: HTMLImageElement } = {};
 var ownMouse: RemoteObject&ExampleMouse;
 
 body.onmousemove = function(ev) {
-  console.log(ownMouse.__remoteInstance.id, ownMouse.__remoteInstance.own);
   ownMouse.x = ev.clientX;
   ownMouse.y = ev.clientY;
 }
@@ -28,8 +25,7 @@ body.onmousemove = function(ev) {
 // Create an new image when a client joins the room
 c.on('newObject', (obj: RemoteObject) => {
   if (obj instanceof ExampleMouse) {
-    console.log(obj);
-    if (obj.__remoteInstance.own === true) {
+    if (rUtils.GetOwn(obj)) {
         ownMouse = obj;
         return;
     }
@@ -43,14 +39,11 @@ c.on('newObject', (obj: RemoteObject) => {
 
     img.src = 'http://icons.iconarchive.com/icons/icons8/windows-8/16/Very-Basic-Cursor-icon.png';
 
-    images[obj.__remoteInstance.id] = img;
+    // Subscribe to changes. Every time this item changes, lets change
+    // the cursor's image
+    rUtils.Subscribe(obj, (o) => {
+      img.style.left = ""+o.x;
+      img.style.top = ""+o.y;
+    });
   }
 })
-// Update image's position when got new data
-  .on('objectChange', (obj: RemoteObject) => {
-  if (obj instanceof ExampleMouse) {
-    console.log(obj);
-    images[obj.__remoteInstance.id].style.left = ""+obj.x;
-    images[obj.__remoteInstance.id].style.top = ""+obj.y;
-  }
-});
